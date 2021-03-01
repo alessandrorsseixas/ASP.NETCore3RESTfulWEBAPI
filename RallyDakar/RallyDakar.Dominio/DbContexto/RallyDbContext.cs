cbebee556532af6464327a4330bc65f8b483496a
Alessandro Seixas <alessandro.seixas@gmail.com>
2021-03-01 07:56:13 -0300

    36. Adicionando o Contexto de Banco de Dados
    - Escrever o source na classe RallyDbContext

---
+++ RallyDakar/RallyDakar.Dominio/DbContexto/RallyDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RallyDakar.Dominio.Entidades;
+
+namespace RallyDakar.Dominio.DbContexto
+{
+    public class RallyDbContext:DbContext
+    {
+        //Configurando os DbSets
+        public DbSet<Temporada> Temporadas { get; set; }
+        public DbSet<Equipe> Equipes { get; set; }
+        public DbSet<Piloto> Pilotos { get; set; }
+        public DbSet<Telemetria> Telemetrias { get; set; }
+
+        public RallyDbContext(DbContextOptions<RallyDbContext> options):base(options)
+        {
+
+        }
+    }
+}
