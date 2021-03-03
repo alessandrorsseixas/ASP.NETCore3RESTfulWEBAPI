9a735b70fc8713332d1307c95538ecd04094f876
Alessandro Seixas <alessandro.seixas@gmail.com>
2021-03-03 05:05:50 -0300

    39. Adicionando um Controller
    - Criar a controller PilotoController herdando de ControllerBase
    - Criar uma variável readonly da interface IPilotoRepositorio e depois criar o metodo construtor. o AspNet.Core será responsável por passar a instancia do PilotoRepositorio a controller para cada requisição
    
    - Criar um metodo com verbor HttpGet para retornar todos os pilotos

---
+++ RallyDakar/RallyDaka.API/Controllers/PilotoController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RallyDakar.Dominio.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RallyDaka.API.Controllers
+{
+
+    [ApiController]
+    [Route("api/produtos")]
+    public class PilotoController : ControllerBase 
+    {
+        private readonly IPilotoRepositorio _pilotoRepositorio;
+
+        public PilotoController(IPilotoRepositorio pilotoRepositorio)
+        {
+            _pilotoRepositorio = pilotoRepositorio;
+        }
+
+        [HttpGet]
+        public IActionResult ObterTodos()
+        {
+            return Ok("Retornou com sucesso");
+
+        }
+    }
+}
